eef2f89d557d0d23e834dd38277a5951535e3829
Dev Patidar <63471580+develop084@users.noreply.github.com>
2022-06-12 05:36:40 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.set('view engine','ejs');
+
+
+
+app.get('/',(req,res)=>{
+  res.render('index',{
+    title: 'my home page'
+  });
+})
+
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    title: 'this is about page'
+  });
+})
+
+app.get('/download',(req,res)=>{
+  res.download(path.resolve(__dirname)+"/index.html")
+})
+app.listen(PORT,()=>{
+  console.log(`Server Started on Port ${PORT}`);
+})
+
